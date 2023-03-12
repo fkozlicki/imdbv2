9b24081b72c4ff30a1fbb2855806112c113e0212
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-03-12 17:47:08 +0100

    Created CastPreview component

---
+++ src/components/pages/movie/CastPreview.tsx
@@ -0,0 +1,95 @@
+import { Avatar, Box, Typography } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+interface CastPreviewProps {
+	id: number;
+	photo: string;
+	name: string;
+	character: string;
+	episodes?: number;
+}
+
+const CastPreview = ({
+	id,
+	photo,
+	name,
+	character,
+	episodes,
+}: CastPreviewProps) => {
+	return (
+		<Link href={`/person/${id}`}>
+			<Box
+				sx={{
+					display: { md: 'flex' },
+					alignItems: { md: 'center' },
+					gap: { md: '16px' },
+				}}
+			>
+				<Box
+					sx={{
+						paddingBottom: { xs: '100%', md: '96px' },
+						position: 'relative',
+						width: { xs: '100%', md: '96px' },
+						marginBottom: '10px',
+					}}
+				>
+					{photo ? (
+						<Image
+							src={`http://image.tmdb.org/t/p/w500/${photo}`}
+							alt=""
+							fill
+							style={{
+								objectFit: 'cover',
+								borderRadius: '100%',
+								objectPosition: 'top',
+							}}
+						/>
+					) : (
+						<Avatar
+							sx={{
+								width: '100%',
+								height: '100%',
+								position: 'absolute',
+								top: 0,
+								left: 0,
+								fontSize: '96px',
+							}}
+						>
+							<PersonIcon fontSize="inherit" />
+						</Avatar>
+					)}
+				</Box>
+				<Box>
+					<Typography
+						fontSize={16}
+						fontWeight={600}
+						sx={{ textAlign: { xs: 'center', md: 'start' } }}
+					>
+						{name}
+					</Typography>
+					<Typography
+						fontSize={12}
+						color="GrayText"
+						sx={{ textAlign: { xs: 'center', md: 'start' } }}
+					>
+						{character}
+					</Typography>
+					{episodes && (
+						<Typography
+							fontSize={12}
+							color="GrayText"
+							sx={{ textAlign: { xs: 'center', md: 'start' } }}
+						>
+							Episodes: {episodes}
+						</Typography>
+					)}
+				</Box>
+			</Box>
+		</Link>
+	);
+};
+
+export default CastPreview;
