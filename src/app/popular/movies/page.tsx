e87fdd00023726b69552a1d14541b97dec8919b9
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-03-12 17:44:43 +0100

    Created PopularMovies page

---
+++ src/app/popular/movies/page.tsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import React from 'react';
+import MoviesTable from '@/components/MoviesTable';
+
+const getPopularMovies = (page: number) =>
+	axios.get('https://api.themoviedb.org/3/movie/popular', {
+		params: {
+			api_key: process.env.API_KEY,
+			page,
+		},
+	});
+
+const PopularMovies = async () => {
+	const results = await Promise.all([
+		getPopularMovies(1),
+		getPopularMovies(2),
+		getPopularMovies(3),
+		getPopularMovies(4),
+		getPopularMovies(5),
+	]);
+
+	const movies = results.map((result) => result.data.results).flatMap((e) => e);
+
+	return <MoviesTable data={movies} />;
+};
+
+export default PopularMovies;
