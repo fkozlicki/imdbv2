2c03adb38026ae103eeeb35e3a429b3c8db3c76a
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-03-12 17:51:12 +0100

    Created SectionTitle component

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,38 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React from 'react';
+
+interface SectionTitleProps {
+	title: string;
+}
+
+const SectionTitle = ({ title }: SectionTitleProps) => {
+	return (
+		<Box
+			paddingLeft="16px"
+			marginBottom={1.5}
+			sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}
+		>
+			<Box
+				sx={{
+					width: '4px',
+					height: { xs: '24px', sm: '32px' },
+					background: 'yellow',
+				}}
+			></Box>
+			<Typography
+				fontWeight={600}
+				sx={{
+					fontSize: {
+						xs: '24px',
+						sm: '32px',
+					},
+				}}
+			>
+				{title}
+			</Typography>
+		</Box>
+	);
+};
+
+export default SectionTitle;
