2ebc9b20793682502d5a0ce67a0a718d633e712e
Filip Koźlicki <filip.kozlickii@gmail.com>
2023-03-12 17:48:46 +0100

    Created About component

---
+++ src/components/pages/person/About.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { Box, Card, Container, Typography } from '@mui/material';
+import Image from 'next/image';
+import React from 'react';
+
+interface AboutProps {
+	name: string;
+	image: string;
+	biography: string;
+	birthday: string;
+}
+
+const About = ({ name, image, biography, birthday }: AboutProps) => {
+	return (
+		<Card sx={{ paddingBlock: 4, marginBottom: 4 }}>
+			<Container>
+				<Typography
+					sx={{
+						fontSize: {
+							xs: '32px',
+							sm: '48px',
+						},
+					}}
+				>
+					{name}
+				</Typography>
+				<Box sx={{ display: 'flex', gap: 6 }}>
+					<Box sx={{ width: 'calc(30%)' }}>
+						<Box
+							sx={{
+								paddingBottom: '148%',
+								position: 'relative',
+								width: '100%',
+							}}
+						>
+							<Image
+								src={`http://image.tmdb.org/t/p/w500/${image}`}
+								alt=""
+								fill
+							/>
+						</Box>
+					</Box>
+					<Box sx={{ width: '70%' }}>
+						<Typography fontSize={18} fontWeight={600} marginBottom={1}>
+							Biography:
+						</Typography>
+						<Typography marginBottom={2}>{biography}</Typography>
+						<Typography>
+							<Typography component="span" fontWeight={600} marginRight={1}>
+								Born
+							</Typography>
+							{new Intl.DateTimeFormat('en-US', {
+								month: 'long',
+								day: '2-digit',
+								year: 'numeric',
+							}).format(new Date(birthday))}
+						</Typography>
+					</Box>
+				</Box>
+			</Container>
+		</Card>
+	);
+};
+
+export default About;
